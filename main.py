751ab81c0d58d813e4e1d9994c599e02f6c87441
Lasantha <lasantha1130@gmail.com>
2022-09-23 00:24:57 +0530

    implement text to speech using google text to speech api

--- main.py
+++ main.py
@@ -1,15 +1,56 @@
 from PyPDF2 import PdfReader
 import sys
+from google.cloud import texttospeech
+import random
 
 
 def main():
     # pdf = sys.argv[1]
+    # reader = PdfReader(pdf)
+
     reader = PdfReader("sample.pdf")
     number_of_pages = len(reader.pages)
 
     for page_number in range(number_of_pages):
         page = reader.pages[page_number]
-        print(page.extract_text())
+    #     print(page.extract_text())
+
+    """Synthesizes speech from the input string of text or ssml.
+    Make sure to be working in a virtual environment.
+
+    Note: ssml must be well-formed according to:
+        https://www.w3.org/TR/speech-synthesis/
+    """
+
+    # Instantiates a client
+    client = texttospeech.TextToSpeechClient()
+
+    # Set the text input to be synthesized
+    synthesis_input = texttospeech.SynthesisInput(text="Hello, World!")
+
+    # Build the voice request, select the language code ("en-US") and the ssml
+    # voice gender ("neutral")
+    voice = texttospeech.VoiceSelectionParams(
+        language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+    )
+
+    # Select the type of audio file you want returned
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3
+    )
+
+    # Perform the text-to-speech request on the text input with the selected
+    # voice parameters and audio file type
+    response = client.synthesize_speech(
+        input=synthesis_input, voice=voice, audio_config=audio_config
+    )
+
+    # The response's audio_content is binary.
+    random_number = random.randint(1, 1000)
+    with open(f"output{random_number}.mp3", "wb") as out:
+        # Write the response to the output file.
+        out.write(response.audio_content)
+        print(f'Audio content written to file "output{random_number}.mp3"')
 
 
 main()
