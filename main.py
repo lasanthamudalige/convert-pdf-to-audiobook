878315ea4db047b069ad07642981090b473a5770
Lasantha Mudalige <lasantha1130@gmail.com>
2022-12-19 23:05:18 +0530

    change the whole program to work on a new package as i lost the key for the old program

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import PyPDF2
+from gtts import gTTS
 import sys
-from google.cloud import texttospeech
-
-
-"""Free google speech to text is limited to 5000 characters"""
+import random
 
 
 def main():
@@ -23,54 +21,22 @@ def main():
                 page = reader.pages[page_number]
                 text += page.extract_text()
 
-            """Synthesizes speech from the input string of text or ssml.
-            Make sure to be working in a virtual environment.
+            # Language in which you want to convert
+            language = 'en'
 
-            Note: ssml must be well-formed according to:
-                https://www.w3.org/TR/speech-synthesis/
-            """
+            # Passing the text and language to the engine,
+            # here we have marked slow=False. Which tells
+            # the module that the converted audio should
+            # have a high speed
+            audio_object = gTTS(text=text, lang=language, slow=False)
 
-            # Instantiates a client
-            client = texttospeech.TextToSpeechClient()
+            # Saving the converted audio in a mp3 file with the file name
+            pdf_name = pdf.split(".")[0]
 
-            # Set the text input to be synthesized
-            synthesis_input = texttospeech.SynthesisInput(text=text)
+            audio_object.save(f"{pdf_name}.mp3")
 
-            # Build the voice request, select the language code ("en-US") and the ssml
-            # voice gender ("neutral")
-            voice = texttospeech.VoiceSelectionParams(
-                language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.SSML_VOICE_GENDER_UNSPECIFIED
-            )
-
-            # Select the type of audio file you want returned
-            audio_config = texttospeech.AudioConfig(
-                audio_encoding=texttospeech.AudioEncoding.MP3
-            )
-
-            # Perform the text-to-speech request on the text input with the selected
-            # voice parameters and audio file type
-            response = client.synthesize_speech(
-                input=synthesis_input, voice=voice, audio_config=audio_config
-            )
-
-            # Split name from the '.' and get first item from the list
-            audio_book = pdf.split(".")[0]
-
-            # If there the file is on a different location split it with forward slash
-            audio_book_name = audio_book.split("/")
-
-            if audio_book_name != "":
-                audio_book_name = audio_book_name[-1]
-            # If the file is in the same directory
-            else:
-                audio_book_name = audio_book
-
-            # The response's audio_content is binary.
-            with open(f"{audio_book_name}.mp3", "wb") as out:
-                # Write the response to the output file.
-                out.write(response.audio_content)
-                print(f'Audio content written to file "{audio_book_name}.mp3"')
-                sys.exit(0)
+            # Show the complete message
+            print(f"{pdf_name}.mp3 saved successfully")
 
         except PyPDF2.errors.PdfReadError:
             print("Invalid file type")
